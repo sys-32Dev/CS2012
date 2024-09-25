7ab40f7dab4488ca6c84218039ab0180f99c3ab4
sys_32 <gamer1912real@gmail.com>
2024-09-25 12:39:03 -0700

    Add files via upload

---
+++ cookies/FortuneCookie.java
@@ -0,0 +1,13 @@
+
+package cookies;
+public class FortuneCookie {
+    private final String message;
+
+    public FortuneCookie(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
